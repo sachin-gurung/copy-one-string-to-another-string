0a4220d47515598f03f396b99d696d47dc12c6c3
Sachin Gurung <saching061@hotmail.com>
2019-12-10 22:59:45 -0800

    first commit

--- copy one string to another string/Program.cs
+++ copy one string to another string/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace copy_one_string_to_another_string
+public class Exercise8
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string str1;
+        int i, l;
+
+        Console.Write("\n\nCopy one string into another string :\n");
+        Console.Write("-----------------------------------------\n");
+        Console.Write("Input the string : ");
+        str1 = Console.ReadLine();
+
+        l = str1.Length;
+        string[] str2 = new string[l];
+
+        /* Copies string1 to string2 character by character */
+        i = 0;
+        while (i < l)
         {
+            string tmp = str1[i].ToString();
+            str2[i] = tmp;
+            i++;
         }
+        Console.Write("\nThe First string is : {0}\n", str1);
+        Console.Write("The Second string is : {0}\n", string.Join("", str2));
+        Console.Write("Number of characters copied : {0}\n\n", i);
     }
 }
+/*
+Copy one string into another string :
+-----------------------------------------
+Input the string : good morning
+
+The First string is : good morning
+The Second string is : good morning
+Number of characters copied : 12
+
+Press any key to continue . . .
+
+*/
